d707946dc0a2abf02faad571d94c8255b00df5fa
adi <kubatbekov>
2020-12-01 11:59:03 +0600

    fixed router

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>This is an about page</h2>
+    <h3>This is an about page</h3>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'About',
+}
+</script>
