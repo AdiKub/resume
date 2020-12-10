aa0c51f2fa7be42b4af0874072210e1590b5ae28
AdiKub <adiletkub@gmail.com>
2020-12-10 10:01:59 +0600

    fixed modal and sidefooter

--- src/components/SideFooter.vue
+++ src/components/SideFooter.vue
@@ -3,26 +3,7 @@
     <div class="row">
       <div class="col-lg-5">
         <div class="bs-dashboard-content--inner">
-          <div class="bs-dashboard-content--header row justify-content-between">
-            <div class="col-auto column col-left">
-              <button
-                @click="sendPaymentLink()"
-                class="bs-btn btn-fix-action bs-btn-line-action"
-              > ADD CARD
-              </button>
-            </div>
-       
-          </div>
-          <div class="bs-dashboard-content--header row justify-content-between">
-            <div class="col-auto column col-left">
-              <button
-                @click="makeAuthLink()"
-                class="bs-btn btn-fix-action bs-btn-line-action"
-              > MAKE LINK 
-              </button>
-            </div>
-       
-          </div>
+         
         </div>
       </div>
     </div>
@@ -40,46 +21,15 @@ export default {
   computed: {
      ...mapState('auth', {
       stylistId: state => state.user.sylistId
-    }),
-     ...mapState('users', {
-      stylistById: state => state.getUserById.user.stylist,
-      stylistEmail: state => state.getUserById.user.email
-    }),
+    })
   },
   watch: {
-    'stylistId' () {
-      this.md5Id = this.makeMd5(this.stylistId)
-    },
-    'stylistById' () {
-      const stylistId = this.getStylistId(this.stylistById)
-      this.md5Id = this.makeMd5(stylistId)
-    }
+ 
   },
   mounted() {
     this.md5Id = this.makeMd5(this.stylistId || this.getStylistId(this.stylistById))
   },
   methods: {
-    sendPaymentLink() {
-      const url = `https://connect.stripe.com/express/oauth/authorize?client_id=ca_HiwkZNIUbmgdij9LCrIwL7lWU2kSssRF&state=${this.md5Id}&suggested_capabilities[]=transfers&stripe_user[email]=${this.stylistEmail}&redirect_uri=https://beautybookbackend.demo.sibers.com/api/stripe/oauth#/`
-      this.md5Id && this.stylistEmail && (window.open(url,'_blank'))
-    },
-    async makeAuthLink() {
-      const getUrl = window.location;
-      const baseUrl = getUrl .protocol + "//" + getUrl.host;
-      const id = this.stylistId || this.getStylistId(this.stylistById)
-      const link = `${baseUrl}/sign-up?stylistId=${id}`
-
-      await this.$store.dispatch('auth/setRegisterLink', link)
-      this.$modal.show('show-link')
-    },
-    makeMd5(id) { if (id) return   md5(id) },
-
-    getStylistId(stylist) {
-      if (stylist) {
-        const stylistArr = stylist.split('/')
-        return Number(stylistArr[stylistArr.length - 1])
-      }
     }
-  }
 }
 </script>
--- src/components/modals/ModalHer.vue
+++ src/components/modals/ModalHer.vue
@@ -10,25 +10,7 @@
     <div class="bs-modal">
       <div class="bs-modal-header"></div>
       <div class="bs-modal-body">
-        <div class="row justify-content-center">
-          <div class="col-lg-8 bs-modal--content">
-            <div class="appointment-confirmed text-center">
-              <div class="appointment-confirmed--icon">
-                <img src="@/assets/images/svg/check.svg" />
-              </div>
-              <div class="appointment-confirmed--text">
-                <p class="mb-5">
-                  Appointment Confirmed
-                  <br />Friday, APRIL 04 2020
-                </p>
-                <button
-                  @click="goToPage()"
-                  class="bs-btn bs-btn-primary bs-btn-lg bs-btn-full radius-4"
-                >View Your Booking</button>
-              </div>
-            </div>
-          </div>
-        </div>
+       
       </div>
       <div class="bs-modal-footer"></div>
     </div>
@@ -40,12 +22,6 @@ export default {
   name: 'ModalAppointmentConfirmed',
 
   methods: {
-    goToPage() {
-      this.$modal.hide('pick-date-time')
-      this.$modal.hide('appointment-confirmed')
-      this.$modal.hide('confirm-details')
-      this.$router.push(`/client-profile/appointments`)
-    }
   }
 }
 </script>
